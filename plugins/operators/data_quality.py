4ac87650d5886a9a7a8666f75187280046d7b16d
Fan Yuan <yfan941101@gmail.com>
2021-04-23 17:26:17 -0400

    feature: add multiple input for data quality check operator

--- plugins/operators/data_quality.py
+++ plugins/operators/data_quality.py
@@ -13,13 +13,15 @@ class DataQualityOperator(BaseOperator):
     def __init__(self,
                  # Define operators params (with defaults)
                  redshift_conn_id="",
-                 tables=[],
+                 tables=["songplays", "artists", "songs", "users", "time"],
+                 dq_checks=[],
                  *args, **kwargs):
 
         super(DataQualityOperator, self).__init__(*args, **kwargs)
         # Map params
         self.redshift_conn_id = redshift_conn_id
         self.table = tables
+        self.dq_checks = dq_checks
 
     def execute(self, context):
         logging.info('Start running data quality check')
@@ -34,4 +36,21 @@ class DataQualityOperator(BaseOperator):
             if num_records < 1:
                 raise ValueError(f"Data quality check failed. {self.table[i]} contained 0 rows")
             logging.info(f"Data quality on table {self.table[i]} check passed with {records[0][0]} records")
+            
+            # check the input sql queries
+            for query in self.dq_checks:
+                sql = check.get('check_sql')
+                exp_result = check.get('expected_result')
+
+                records_query = redshift.get_records(sql)[0]
+
+                if exp_result != records_query[0]:
+                    error_count += 1
+                    failing_tests.append(sql)
+ 
+            if error_count > 0:
+                self.log.info('Tests failed:')
+                self.log.info(failing_tests)
+                raise ValueError('Data quality check failed')
+
         
